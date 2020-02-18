af76db0bbcf305e4a40f8d91956c00f1e02d5040
Muzahid037 <48602232+Muzahid037@users.noreply.github.com>
2020-02-18 14:08:38 +0600

    18/02/20

--- CodeForces/A. Erasing Zeroes.cpp
+++ CodeForces/A. Erasing Zeroes.cpp
@@ -1,1 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
 
+vector<int>cnt;
+
+int main()
+{
+
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s;
+        cin>>s;
+
+        int ln=s.size();
+        int i=0,j=0;
+        while(i<ln-1)
+        {
+            if(s[i]==0){i++;}
+            else if(s[i]==1 && s[i+1]==1){i++;}
+            else if(s[i]==1 && s[i+1]==0)
+            {
+                i++;
+                while(s[i]==0)
+                {
+                    cnt[j]++;
+                }
+                j++;
+            }
+        }
+        for(vector<int>::iterator it=cnt.begin();it!=cnt.end();it++)
+        {
+            cout<<*it<<endl;
+        }
+
+
+    }
+
+
+    return 0;
+}
